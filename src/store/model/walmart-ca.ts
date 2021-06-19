53da1deb09f60c98a3ab9b32c2959735e4ee2ecd
alexisbergeron22 <abergeron-nadeau@dept-info.crosemont.quebec>
2021-06-19 10:30:42 -0400

    amélioration

--- src/store/model/walmart-ca.ts
+++ src/store/model/walmart-ca.ts
@@ -51,6 +51,69 @@ export const WalmartCa: Store = {
       url:
         'https://www.walmart.ca/en/ip/Corsair-SF-Series-SF600-600-Watt-80-PLUS-Gold-Certified-High-Performance-SFX-PSU/PRD6VH8WNKHD36Q',
     },
+    {
+      brand: 'zotac',
+      model: 'twin edge oc',
+      series: '3070',
+      url:
+        'https://www.walmart.ca/en/ip/ZOTAC-GAMING-GeForce-RTX-3070-Twin-Edge-OC-8GB-GDDR6-256-bit-14-Gbps-PCIE-4-0-Gaming-Graphics-Card-IceStorm-2-0-Advanced-Cooling-White-LED-Logo-Light/PRD6OJB1XLMHIGX',
+    },
+    {
+      brand: 'zotac',
+      model: 'twin edge oc white',
+      series: '3070',
+      url:
+        'https://www.walmart.ca/en/ip/ZOTAC-GeForce-RTX-3070-OC-white-edition-ZT-A30700J-10P/PRD2AGBFGJQXPLM',
+    },
+    {
+      brand: 'msi',
+      model: 'ventus 3x oc',
+      series: '3070',
+      url:
+        'https://www.walmart.ca/en/ip/MSI-RTX-3070-VENTUS-3X-OC-8GB-GDDR6-VIDEO-CARD-G3070V3XC/PRD74FM3DBB0WIT',
+    },
+    {
+      brand: 'zotac',
+      model: 'trinity oc',
+      series: '3080',
+      url:
+        'https://www.walmart.ca/en/ip/ZOTAC-GAMING-RTX-3080-Trinity-OC-10G-DDR6-ZT-A30800J-10P/PRD1ACNDWNAL0AP',
+    },
+    {
+      brand: 'zotac',
+      model: 'trinity',
+      series: '3080',
+      url:
+        'https://www.walmart.ca/en/ip/ZOTAC-RTX-3080-TRINITY-AMP-HOLO-10G-DDR6-VIDEO-CARD-ZT-A30800F-10P/PRD19645WJ5M3CF',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'aorus xtreme',
+      series: '3080',
+      url:
+        'https://www.walmart.ca/en/ip/GIGABYTE-AORUS-GeForce-RTX-3080-XTREME-Graphics-Card-DirectX-12-10GB-320-Bit-GDDR6X-PCI-Express-4-0-MAX-COVERED-Cooling-2-x-HDMI-2-1-1-2-0-3-DisplayP/PRD5AIO841H9CRM',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'aorus xtreme',
+      series: '3080',
+      url:
+        'https://www.walmart.ca/en/ip/GIGABYTE-AORUS-GeForce-RTX-3080-XTREME-Graphics-Card-10GB-320-Bit-GDDR6X-PCIe-4-0-MAX-COVERED-Cooling-2-x-HDMI-2-1-1-2-0-3-DisplayPort-1-4a-w-Mytrix-/PRD5YPINVJ7NC3O',
+    },
+    {
+      brand: 'asus',
+      model: 'tuf oc',
+      series: '3080',
+      url:
+        'https://www.walmart.ca/en/ip/ASUS-TUF-Gaming-NVIDIA-GeForce-RTX-3080-OC-Edition-Graphics-Card-PCIe-4-0-10GB-GDDR6X-HDMI-2-1-DisplayPort-1-4a-Dual-Ball-Fan-Bearings/PRD1EBS7N2O9U7B',
+    },
+    {
+      brand: 'zotac',
+      model: 'trinity oc',
+      series: '3090',
+      url:
+        'https://www.walmart.ca/en/ip/Zotac-RTX-3090-Trinity-OC-24GD6-ZT-A30900J-10P/PRD77XWS7MKIUBR',
+    },
   ],
   name: 'walmart-ca',
 };
